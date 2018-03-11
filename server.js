3c6ac76e8d46961755fc77938c6b25184ff22945
Glitch (truthful-truck) <none>
2018-03-11 09:06:22 +0000

    👂🕴 Checkpoint server.js:495043/68

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 function cl(x){console.log(x)}
 var express = require('express');
 var app = express();
+// var upload = multer({ dest: 'uploads/' });
 
 app.get("/", function (request, response) {
   //response.sendFile(__dirname + '/views/index.html');
-  response.send("<form action='/upload'><input name='bob' value='fred' /><button type='button'>upload file</button><button type='submit'>submit</button></form>");
+  response.send("<form action='/upload' method='post'><button type='button'>upload file</button><button type='submit' name='bob' value='fred'>submit</button></form>");
 });
 
-app.get("/upload", function (request, response) {
+/*
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
+})
+*/
+
+app.post("/upload", function (request, response) {
   cl(request.params);
   response.send(request.params.bob);
 });
