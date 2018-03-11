51ae8260d78fb1e31e1f9cf6a5c9cf09232657dd
Glitch (truthful-truck) <none>
2018-03-11 08:45:19 +0000

    🏢👁 Checkpoint server.js:495043/178

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  //response.sendFile(__dirname + '/views/index.html');
+  response.send("<form><button type='button'>upload file</button><button type='submit'>submit</button></form>");
 });
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
